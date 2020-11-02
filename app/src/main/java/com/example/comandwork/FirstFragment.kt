f0041640c2fcc9df31c59062da9edd73cfd1135d
Vladislav <kudvldsv@gmail.com>
2020-11-03 00:25:36 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/comandwork/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.example.comandwork
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_fisrt.*
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fisrt, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        nextButton.setOnClickListener() {
+            val firstWord = firstInput.text.toString()
+
+            val fragment = SecondFragment()
+            fragment.arguments = Bundle().also {
+                it.putString("word", firstWord)
+            }
+
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.fragmentContainer, SecondFragment())
+                ?.addToBackStack(null)
+                ?.commit()
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/comandwork/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.comandwork
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_fisrt.view.*
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewFistWord.text = arguments?.getString("word")
+
+    }
+
+}
