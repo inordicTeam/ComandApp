3d671c39c702e6e57b894e794e374e33658216de
Andrey9595 <anb95.95@mail.ru>
2020-11-07 20:00:18 +0800

    commit

--- app/src/main/java/com/example/comandwork/FourthFragments.kt
+++ app/src/main/java/com/example/comandwork/FourthFragments.kt
@@ -5,31 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_second.*
+import kotlinx.android.synthetic.main.fragment_fourth.*
+
 import kotlinx.android.synthetic.main.fragment_third.*
 
-class ThirdFragment: Fragment() {
+class FourthFragments : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        return inflater.inflate(R.layout.fragment_fourth,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        resultSecondWorld.text = arguments?.getString("secondWord")
+        resultThirdWorld.text = arguments?.getString("FourthWorld")
 
         backButtons.setOnClickListener { fragmentManager?.popBackStack()  }
         thirdButtonOK.setOnClickListener {
-            val thirdWorld = thirdInput.text.toString()
-            val fragmentThird = FourthFragments()
-            fragmentThird.arguments = Bundle().also {
-                it.putString("ThirdWorld", thirdWorld )
+            val forurthWorld = thirdInput.text.toString()
+            val fragmentFourth = FourthFragments()
+            fragmentFourth.arguments = Bundle().also {
+                it.putString("World", forurthWorld)
             }
-
+                //git
             fragmentManager?.beginTransaction()
-                ?.replace(R.id.fragmentContainer, fragmentThird)
+                ?.replace(R.id.fragmentContainer, fragmentFourth)
                 ?.addToBackStack(null)
                 ?.commit()
         }
